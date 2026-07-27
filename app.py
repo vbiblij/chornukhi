28a78d97bafbae32cb49f2072f9bbe80471b8abf
vbiblij <vbiblij@gmail.com>
2026-07-27 14:44:53 +0300

    Виправив назву змінної Flask на app

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import time
 from data import HROMADA_CENTER, SETTLEMENTS
 from map_process import MapProcess
 import sys
-from __future__ import annotations
+
 import threading
 from flask import Flask, Response, render_template_string, jsonify
 from core.process import Process
